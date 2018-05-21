7edb1a14d8bc618255d9715df5d6d1ae54c1d468
wukia <wukai@doumi.com>
2018-05-21 08:23:51 +0800

    '异步加载'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// import Recommend from 'components/recommend/recommend'
+// import Singer from 'components/singer/singer'
+// import Rank from 'components/singer/singer'
+// import Search from 'components/search/search'
+// import SingerDetail from 'components/singer-detail/singer-detail'
+// import Disc from 'components/disc/disc'  
+// import TopList from 'components/top-list/top-list'
+// import UserCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
 
@@ -49,8 +56,8 @@ const TopList = (resolve) => {
 }
 
 const UserCenter = (resolve) => {
-    import ('components/user-center/user-center').then((module) => {
-        resolve(module)
+            resolve(module)
+        import ('components/user-center/user-center').then((module) => {
     })
 }
 
