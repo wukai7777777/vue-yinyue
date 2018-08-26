94759c94f21fb516679cf5981d9e7ec1c0b1c2a1
wukai7777777 <1622186950@qq.com>
2018-08-26 16:29:37 +0800

    test need-build-dek

--- src/store/actions.js
+++ src/store/actions.js
@@ -133,3 +133,4 @@ export const saveFavoriteList = function({commit}, song) {
 export const deleteFavoriteList = function({commit}, song) {
     commit(types.SET_FAVORITE_LIST, deleteFavorite(song))
 }
+
